8f32d40b9621409c10e3376e6c1685562b3e26ee
Farah Masood <farah.masood@sjsu.edu>
2023-04-25 12:23:58 -0700

    about

--- about.php
+++ about.php
@@ -13,6 +13,18 @@
     <h1 id="tester">About Us</h1>
     <hr />
 
+    <h3 class="headers">What is [APP NAME]</h3>
+    <p class="text">[APP NAME] is a calorie tracking application that provides an easy way to visualize and manage your
+        diet. Calorie counting is beneficial in maintaining a healthy lifestyle when working out or managing weight. It
+        is important to use [APP NAME] to help reach your goal of calories eaten everyday. Calorie counting can also
+        help educate users and help them make better choices about the food they consume. </p>
+
+    <h3 class="headers">How to Use [APP NAME]?</h3>
+    <p class="text"> Welcome to our application! New users must create an account by navigating to the Login/Sign Up
+        tab. After creating an account, all users will be able to login to the app. After logging in, users will be able
+        to add a goal and add meals to track calorie amount.
+    </p>
+
 
 </body>
 
