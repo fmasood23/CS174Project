bd5cbb291311e0ccfda67defacf0fae3df971ef1
Farah Masood <farah.masood@sjsu.edu>
2023-04-28 17:53:11 -0700

    edit to update pass and added goal implementation

--- profile.php
+++ profile.php
@@ -13,54 +13,33 @@
     <?php
     if (isset($_COOKIE['logged_in']) && $_COOKIE['logged_in'] == "true") {
         ?>
-        <?php
-        include 'mysql_connector.php';
-        include 'user_functions.php';
-        ?>
+
         <div id="profile">
             <img src="https://cdn-icons-png.flaticon.com/512/2948/2948035.png" />
             <p id="username">
                 <?php echo $_COOKIE['username'] ?>
             </p>
             <div id="goal_wrapper">
-                <p id="dailyGoal">Daily Goal: </p>
-                <form method="post">
-                    <input id="change" type="submit" name="changeGoal" value="Change" />
-                </form>
+                <p id="dailyGoal"><strong>Daily Goal: </strong>
+                    <?php
+                    $val = "";
+                    global $conn;
+                    include 'mysql_connector.php';
+                    include 'goal_functions.php';
+                    $val .= getGoal($conn, $_COOKIE['username']);
+                    $val .= " calories";
+                    echo $val; ?>
+                </p>
+                <br />
             </div>
 
-            <form method="post">
-                <input class="button" type="submit" name="change_pass" value="Change Password" />
+            <form method="post" action="edit_profile.php">
+                <input class="button" type="submit" name="change_profile" value="Edit Profile" />
             </form>
 
-            <?php
-            if (isset($_POST['change_pass'])) {
-                ?>
-                <form action="profile.php" method="post">
-                    <div id="change_pass_form">
-
-                        <label for="password">Enter a New Password: </label>
-                        <input type="password" placeholder="Enter New Password" name="new_pass" required>
-
-                        <button type="submit" name="update">Change Password</button>
-                    </div>
-
-                    <?php
-                    global $conn;
-                    if (isset($_POST['update'])) {
-                        $get_user = $_COOKIE['username'];
-                        $get_pass = $_POST['new_pass'];
-
-                        $updatePass = updatePassword($conn, $get_user, $get_pass);
-                        if ($updatePass === TRUE) {
-                            header("location: profile.php");
-                        }
-                    } ?>
-                <?php } ?>
-
-                <form method="post">
-                    <input class="button" type="submit" name="signout" value="Sign Out" />
-                </form>
+            <form method="post">
+                <input class="button" type="submit" name="signout" value="Sign Out" />
+            </form>
         </div>
 
         <?php
