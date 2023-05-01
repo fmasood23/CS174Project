28af5826cc42c23547230d52dacca3d6add67c54
Nimra <nimnaveed@gmail.com>
2023-04-30 17:13:33 -0700

    queries for calories and meals

---
+++ calories_functions.php
@@ -0,0 +1,28 @@
+<?php
+
+
+
+//add calories
+funtion addCalories($conn, $username, $date, $calories_added)
+{
+    $query = "INSERT INTO Calories (username, date, calories_added) VALUES (?, ?, ?)";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("sss", $username, $date, $calories_added);
+    $result = $stmt->execute();
+
+    return $result;
+}
+//update Calories
+function updateCalories($conn, $username, $date, $calories_added)
+{
+    $query = "UPDATE Calories SET calories_added = ? WHERE username = ?";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("ssss", $calories_added, $username);
+    $result = $stmt->execute();
+
+    return $result;
+}
+
+
+
+?>
---
+++ meals_functions.php
@@ -0,0 +1,20 @@
+<?php
+//add name
+//add serving size
+//add calories
+
+funtion addMealInfo($conn, $name, $serving_size, $calories)
+{
+    $query = "INSERT INTO Meals (name, serving_size, calories) VALUES (?, ?, ?)";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("sss", $name, $serving_size, $calories);
+    $result = $stmt->execute();
+
+    return $result;
+}
+
+
+
+
+
+?>
