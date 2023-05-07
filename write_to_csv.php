bc66d34c8833bfae1a5cae06c97fe23a7ecf61cf
Quinlan Nguyen <quinlan.nguyen@sjsu.edu>
2023-05-06 19:13:06 -0700

    Added error checking to write_to_csv.php file

--- write_to_csv.php
+++ write_to_csv.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 $foodItem = str_replace('"', '', $_POST['foodItem']);
 $servingSize = $_POST['servingSize'];
 $calorieAmount = $_POST['calorieAmount'];
