7c8bdb65d6479d0b2df2633d67bcf7522b40983e
Ricky Kuang <ricky.kuang10@gmail.com>
2023-05-08 18:30:10 -0700

    added check for food item already in csv

--- write_to_csv.php
+++ write_to_csv.php
@@ -6,6 +6,14 @@ $foodItem = str_replace('"', '', $_POST['foodItem']);
 $servingSize = $_POST['servingSize'];
 $calorieAmount = $_POST['calorieAmount'];
 
+# check if already in CSV
+$csvArrayMap = array_map('str_getcsv', file('food_calories.csv'));
+foreach ($csvArrayMap as $row) {
+    if ($row[0] === $foodItem) {
+        exit();
+    }
+}
+
 $data = array($foodItem, $servingSize, $calorieAmount);
 $file = fopen('food_calories.csv', 'a');
 flock($file, LOCK_EX);
