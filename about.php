9c3c1db146d63eb93fa63469703622fb9db66775
Farah Masood <farah.masood@sjsu.edu>
2023-05-09 17:58:26 -0700

    updated about page

--- about.php
+++ about.php
@@ -15,16 +15,30 @@
 
     <h3 class="headers">What is CalorieSmart?</h3>
     <p class="text">CalorieSmart is a calorie tracking application that provides an easy way to visualize and manage
-        your
-        diet. Calorie counting is beneficial in maintaining a healthy lifestyle when working out or managing weight. It
-        is important to use CalorieSmart to help reach your goal of calories eaten everyday. Calorie counting can also
-        help educate users and help them make better choices about the food they consume. </p>
+        your diet. Our goal in creating this application is to provide a tool for controlling calorie intake
+        effectively. Calorie counting is beneficial in maintaining a healthy lifestyle when working out or managing
+        weight. <br><br>With CalorieSmart, keeping track of calorie goals is made easy for you. This application will
+        help keep
+        you on track to achieving your desired calorie intake. By visualizing calorie progress, CalorieSmart can help
+        you make healthier choices.
+    </p>
 
     <h3 class="headers">How to Use CalorieSmart?</h3>
-    <p class="text"> Welcome to our application! New users must create an account by navigating to the Login/Sign Up
-        tab. After creating an account, all users will be able to login to the app. After logging in, users will be able
-        to add a calorie goal under the Edit Profile page and add meals to track calorie amount under the Calorie
-        Tracker feature.
+    <p class="text"> Welcome to our application!<br><br>New users must create an account by navigating to the Login/Sign
+        Up tab. After creating an account, all users will be able to login to the app. After logging in, users will be
+        directed to the Profile page. <br><br>On the Profile page, users can navigate to edit their profile or view
+        calorie
+        analytics. Under the Edit Profile page, users are able to add a calorie goal under the Edit Profile page or
+        change their password. The Calorie Analytics page displays a graph of calorie intake depending on selected date
+        range. <br><br>On the Calorie Tracker page, Users can filter calorie progress by date to visualize the exact
+        calorie
+        amounts consumed on that date. Users can also keep track of their calorie progress for the current date on the
+        Home
+        tab. The Previous Calorie Intake button provides a way to access and delete previous
+        meals based on the date selected. <br><br> Finally, to maintain calorie progress and update the site, users can
+        add each of their meals for the current day under
+        the Update Meals section of the Calorie tracker page. They can also add custom meals if their meal is not
+        displayed. <br><br>
     </p>
 
 
